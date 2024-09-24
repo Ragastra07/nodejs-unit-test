6c0fe8324492fb2a96ad4e7a00025732f7b993d1
Ragastra07 <haryo.accn.7@gmail.com>
2024-09-24 21:33:43 +0700

    trying use verbose

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,13 @@
 import { sum } from "../src/sum";
 
-test("testing sum function", () => {
+test("testing sum function 1", () => {
     const result = sum(10, 7);
 
     expect(result).toBe(17);
-})
+});
+
+test("testing sum function 2", () => {
+    const result = sum(10, 7);
+
+    expect(result).toBe(17);
+});
