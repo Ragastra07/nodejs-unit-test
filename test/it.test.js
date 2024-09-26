89c3ee84af4205d336f6e370f9ec8fd0e81bc769
Ragastra07 <haryo.accn.7@gmail.com>
2024-09-26 22:37:59 +0700

    it unit test

---
+++ test/it.test.js
@@ -0,0 +1,6 @@
+import { sumAll } from "../src/sum";
+describe("when call sumAll([10, 10, 10])", () =>{
+    it ("should get 30", () =>{
+        expect(sumAll([10, 10, 10, 10])).toBe(40);
+    });
+});
