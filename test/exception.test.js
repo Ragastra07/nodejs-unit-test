703a6e8e09cc215a8c29a27633ff511fb2b0eb5b
Ragastra07 <haryo.accn.7@gmail.com>
2024-09-26 22:13:07 +0700

    exception unit test

--- test/exception.test.js
+++ test/exception.test.js
@@ -3,3 +3,7 @@ import { callMe  } from "../src/exception.js";
 test("exception", () => {
     expect(() => (callMe("tara"))).toThrow();
 });
+
+test("exception error", () => {
+    expect(() => (callMe("taras"))).toBe("OK");
+});
